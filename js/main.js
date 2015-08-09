258b9c7cac19440fb78c42916298397a54d663cd
Vinicius Moraes <vmoraes1990@gmail.com>
2015-08-09 19:33:58 -0300

    Filter State - First Commit

---
+++ js/main.js
@@ -0,0 +1,127 @@
+var json 	= "json/content.json",
+	Filter 	= {},
+	country = [{"AC":"Acre","AL":"Alagoas","AP":"Amapá","AM":"Amazonas","BA":"Bahia","CE":"Ceará","DF":"Distrito Federal","ES":"Espírito Santo","GO":"Goiás","MA":"Maranhão","MT":"Mato Grosso","MS":"Mato Grosso do Sul","MG":"Minas Gerais","PA":"Pará","PB":"Paraíba","PR":"Paraná","PE":"Pernambuco","PI":"Piauí","RJ":"Rio de Janeiro","RN":"Rio Grande do Norte","RS":"Rio Grande do Sul","RO":"Rondônia","RR":"Roraima","SC":"Santa Catarina","SP":"São Paulo","SE":"Sergipe","TO":"Tocantins"}];
+
+Filter = {
+	collection: "",
+	selectContainer: "header",
+	selectState: "filter-state",
+	selectCity: "filter-city",
+	selectMain: "filter-main",
+
+	init : function(){
+		this.get();
+	},
+
+	selected : function(){
+		var disabledCity = $("#" + this.selectCity).find("li")[0] ? true : false;
+		
+		if (disabledCity == false) {
+			$("#" + this.selectCity).addClass('disabled');
+
+			return disabledCity;
+		}else {
+			$("#" + this.selectCity).removeClass('disabled');
+			return disabledCity;
+		}
+	},
+
+	disabled : function(){
+		var list 	= this.collection,
+			_this 	= this;
+
+		_.each(list, function(value, key){
+			var action = $("#" + _this.selectState + " li[data-state='" + value.estado + "'][data-action='true']").length;
+			if(action == 0) {
+				$("#" + _this.selectState + " li[data-state='" + value.estado + "']").attr("data-action", true);
+			}
+			
+		});
+	},
+
+	get : function(){
+		
+		var _this = this;
+
+		$.getJSON(json, function(data){
+			//console.dir(data);
+		}).done(function(data){
+			_this.collection = data;
+			_this.selected();
+			//_this.disabled();
+			_this.clickState();
+			_this.clickCity();
+		});
+	},
+
+	filterAll : function(id){
+		var state 		= _.where(this.collection, { estado: id }),
+			_this		= this,
+			item		= "",
+			listCity	= ""; 
+		
+		_.each(state, function(value, key){
+			item  = "<li>";
+			item += "<h2>" + value.nome + "</h2>";
+			item += "<h3>" + value.endereco + " - " + value.cep + "</h3>";
+			item += "<h4>" + value.bairro + "</h4>";
+			item += "<h4>" + value.cidade + "</h4>";
+			item += "</li>";
+
+			listCity = "<li data-city='" + value.cidade + "'>";
+			listCity += value.cidade;
+			listCity += "</li>";
+
+			$("#" + _this.selectMain).find('.list').append(item);
+			
+			if ($("#" + _this.selectCity).find('li[data-city="' + value.cidade + '"]').length == 0) {
+				$("#" + _this.selectCity).find('.list').append(listCity);
+			}
+
+		});
+	},
+
+	filterBy : function(id){
+		var city 		= _.where(this.collection, { cidade: id }),
+			_this		= this,
+			item		= "",
+			listCity	= ""; 
+
+		_.each(city, function(value, key){
+			item  = "<li>";
+			item += "<h2>" + value.nome + "</h2>";
+			item += "<h3>" + value.endereco + " - " + value.cep + "</h3>";
+			item += "<h4>" + value.bairro + "</h4>";
+			item += "<h4>" + value.cidade + "</h4>";
+			item += "</li>";
+
+			$("#" + _this.selectMain).find('.list').append(item);
+
+		});
+
+	},
+
+	clickState : function(){
+		var _this = this;
+		$(document).on("click", "#" + this.selectState + " li", function(){
+			$("#" + _this.selectMain).find(".list").empty();
+			$("#" + _this.selectCity).find('.list').empty();	
+			var id = $(this).attr("data-state");
+			_this.filterAll(id);
+			_this.selected();
+		});
+	},
+
+	clickCity : function(){
+		var _this = this;
+		$(document).on("click", "#" + this.selectCity + " li", function(){
+			$("#" + _this.selectMain).find(".list").empty();
+			var id = $(this).attr("data-city");
+			_this.filterBy(id);			
+		});	
+	}
+};
+
+(function($){
+	Filter.init();
+})(jQuery);
